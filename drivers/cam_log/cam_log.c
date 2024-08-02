2552af615f331c432d10d7d0664c02b1b48ef7af
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-08-02 15:29:07 +0200

    camera: cam_log: Upgrade to version 2.0
     * from peridot-u-oss
    
    Change-Id: Id21826ad2c8c9eb27dd816609fe0a33f9b499634

--- drivers/cam_log/cam_log.c
+++ drivers/cam_log/cam_log.c
@@ -9,9 +9,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+// MIUI ADD: Camera_CameraSkyNet
 #define DEBUG
 #define pr_fmt(fmt)     KBUILD_MODNAME ": " fmt
-
 #include <linux/init.h>
 #include <linux/ioctl.h>
 #include <linux/fs.h>
@@ -27,119 +27,267 @@
 #include <linux/notifier.h>
 #include <linux/fb.h>
 #include <linux/poll.h>
+#include <linux/kfifo.h>
 #include "cam_log.h"
-
 /*device name after register in charater*/
 #define CAMLOG_DEV_NAME      "camlog"
+#define CAMLOG_DEV_VERSION "2.0"
+static struct camlog_dev  s_read_buf;
+static struct camlog_dev  s_write_buf;
 
-static struct camlog_dev camlogdev;
+static struct mutex camlog_message_lock;
+static  wait_queue_head_t camlog_is_not_empty;
+static dev_t devt;
+static  int debugLevel = 0;
+static struct kfifo message_kfifo;
 
-static long cameralog_ioctl(struct file *filp, unsigned int cmd,
-    unsigned long arg)
-{
-    pr_debug(" cameralog_ioctl!\n");
-    return 0;
+#define MY_IOCTL_TYPE 'M'
+
+#define IOCTL_CMD_LOG_SWITCH _IOW(MY_IOCTL_TYPE, 1, int)
+#define IOCTL_CMD_DUMP _IOW(MY_IOCTL_TYPE, 2, int)
+#define IOCTL_CMD_SET_DEBUG_LEVEL _IOW(MY_IOCTL_TYPE, 3, int)
+#define IOCTL_CMD_READ_KFIFO_CNT _IOR(MY_IOCTL_TYPE, 4, int)
+#define IOCTL_CMD_READ_LAST_MSG _IOR(MY_IOCTL_TYPE, 5, char*)
+#define IOCTL_CMD_SET_DEBUG_ON _IOW(MY_IOCTL_TYPE, 6, int)
+#define IOCTL_CMD_SET_DEBUG_OFF _IOW(MY_IOCTL_TYPE, 7, int)
+
+static uint8_t s_last_message[MESSAGE_MAX];//debug used only
+static uid_t s_last_caller_uid  = 0;
+static pid_t s_last_caller_pid = 0;
+
+static uid_t s_last_reader_uid  = 0;
+static pid_t s_last_reader_pid = 0;
+
+
+static void dump(void) {
+    struct camlog_dev  element;
+    int message_cnt = 0;
+    int i = 0;
+    int ret = 0;
+    pr_debug("----------------- DUMP CAMERALOG START------------------------");
+    pr_debug("cameralog version %s", CAMLOG_DEV_VERSION);
+
+    pr_debug(" last writer uid:%u  pid:%u", s_last_caller_uid, s_last_caller_pid);
+    pr_debug(" last reader uid:%u  pid:%u", s_last_reader_uid, s_last_reader_pid);
+    pr_debug(" last message:%s ",  s_last_message);
+
+    message_cnt = kfifo_len(&message_kfifo);
+    pr_debug(" kfifo len:%d ", message_cnt);
+    if(message_cnt> 0) {
+        pr_debug("-------fifo messages-------");
+        pr_debug("UID\tPID\tMESSAGE");
+        for(i = 0; i < message_cnt; i++) {
+            ret = kfifo_out(&message_kfifo, &element, sizeof(struct camlog_dev));
+            if(ret != sizeof(struct camlog_dev)) {
+                break;
+            }
+            pr_debug("  %u\t%u\t %s\n", element.writer_uid, element.writer_pid,  element.m_camlog_message);
+        }
+        pr_debug("-------------------------------");
+    }
+    pr_debug("----------------- DUMP CAMERALOG END   ------------------------");
+
 }
 
-static int cameralog_open(struct inode *inode, struct file *filp)
-{
+static long cameralog_ioctl(struct file *filp, unsigned int cmd,  unsigned long arg) {
+    pr_debug(" cameralog_ioctl! %u  %lu\n", cmd,  arg);
+    switch (cmd) {
+        case IOCTL_CMD_LOG_SWITCH:
+            {
+                if(0 >= debugLevel) {
+                    debugLevel = 1;
+                } else {
+                    debugLevel = 0;
+                }
+            }
+            break;
+        case IOCTL_CMD_SET_DEBUG_ON:
+            {
+                    debugLevel = 1;
+            }
+            break;
+        case IOCTL_CMD_SET_DEBUG_OFF:
+            {
+                    debugLevel = 0;
+            }
+            break;
+        case IOCTL_CMD_DUMP:
+            {
+                dump();
+            }
+            break;
+        case IOCTL_CMD_SET_DEBUG_LEVEL:
+            {
+                int data;
+                if (copy_from_user(&data, (int *)arg, sizeof(int))) {
+                    return -EFAULT;
+                }
+                debugLevel = data;
+            }
+            break;
+        case IOCTL_CMD_READ_KFIFO_CNT:
+            {
+                int len = kfifo_len(&message_kfifo);
+                if (copy_to_user((int *)arg, &len, sizeof(int))) {
+                    return -EFAULT;
+                }
+            }
+            break;
+        case IOCTL_CMD_READ_LAST_MSG:
+            {
+                if (copy_to_user((char *)arg, s_last_message, strlen(s_last_message) + 1)) {
+                    return -EFAULT;
+                }
+            }
+            break;
+        default:
+            return -ENOTTY;
+    }
+
+    return 0;
+}
+static int cameralog_open(struct inode *inode, struct file *filp) {
     pr_debug(" cameralog_open!\n");
     return 0;
 }
-
 /**
 *
 * @return Number of bytes read.
 */
-static ssize_t cameralog_read(struct file *fp, char __user *buff,
-    size_t length, loff_t *ppos)
-{
+static ssize_t cameralog_read(struct file *fp, char __user *buff,  size_t length, loff_t *ppos) {
     ssize_t bytes_read = 0;
     int ret = 0;
-    bool has_message = false;
-    struct camlog_dev *camlog_dev = &camlogdev;
+    int message_len = 0;
+    struct task_struct *caller_task = current;
 
-    if ( strlen(camlog_dev->m_camlog_message) )
-    {
-        sprintf(camlog_dev->m_camlog_message, "%s\n", camlog_dev->m_camlog_message);
-        has_message = true;
+    if(debugLevel > 0) {
+        pr_debug("cameralog_read .");
+    }
+
+    mutex_lock(&camlog_message_lock);
+    memset(&s_read_buf, 0, sizeof(struct camlog_dev ));
+    if(debugLevel > 0) {
+        pr_debug("cameralog_read  kfifo len:%d",  kfifo_len(&message_kfifo)/sizeof(struct camlog_dev ));
+    }
+
+    ret = kfifo_out(&message_kfifo, &s_read_buf, sizeof(struct camlog_dev ));
+    if (ret != sizeof(struct camlog_dev ) ) {
+        if(debugLevel > 0) {
+            pr_debug("cameralog_read: not read anything from kfifo %d  expect:%d\n", ret, sizeof(struct camlog_dev ));
+        }
+        mutex_unlock(&camlog_message_lock);
+        return 0;
+    }
+
+    message_len =  strlen(s_read_buf.m_camlog_message);
+    if ( message_len > 0 )  {
+        s_read_buf.m_camlog_message[message_len - 1] = '\n';
     } else {
-        has_message = false;
-        strcpy(camlog_dev->m_camlog_message,"\n");
+        mutex_unlock(&camlog_message_lock);
+        return 0;
     }
-    mutex_lock(&camlog_dev->camlog_message_lock);
-
-    ret = copy_to_user(buff, camlog_dev->m_camlog_message,
-                    strlen(camlog_dev->m_camlog_message));
-
-    mutex_unlock(&camlog_dev->camlog_message_lock);
-
-    if ((ret == 0) && has_message)
-    {
-         bytes_read = strlen(camlog_dev->m_camlog_message);
+    if(debugLevel > 0) {
+        pr_debug("cameralog_read message to user:%s   len:%d",s_read_buf.m_camlog_message, message_len );
     }
 
-    memset(camlog_dev->m_camlog_message, 0 , strlen(camlog_dev->m_camlog_message));
+    ret = copy_to_user(buff, s_read_buf.m_camlog_message,  message_len);
 
+    if (ret == 0)    {
+         bytes_read = message_len;
+    }
+    if(debugLevel > 0) {
+        pr_debug("cameralog_read return len:%d", bytes_read );
+    }
+    s_last_reader_uid = from_kuid(&init_user_ns, caller_task->cred->uid);
+    s_last_reader_pid = task_pid_nr(caller_task);
+
+    mutex_unlock(&camlog_message_lock);
     return bytes_read;
 }
-
 void camlog_send_message(void){
-
     pr_debug (" camlog_send_message ss  \n");
 }
 EXPORT_SYMBOL(camlog_send_message);
-
 /**
 *
 * @return number of bytes actually written
 */
-static ssize_t cameralog_write(struct file *fp, const char *buff,
-    size_t length, loff_t *ppos)
+static ssize_t cameralog_write(struct file *fp, const char *buff,   size_t length, loff_t *ppos)
 {
     int ret = 0;
-    ssize_t bytes_read = 0;
-    struct camlog_dev *camlog_dev = &camlogdev;
+    ssize_t bytes_wirte = 0;
+    int valid_len = 0;
+    struct task_struct *caller_task = current;
 
-    mutex_lock(&camlog_dev->camlog_message_lock);
 
-    ret = copy_from_user(camlog_dev->m_camlog_message, buff,
-                                length);
-    if (0 == ret)
-    {
-        bytes_read = strlen(camlog_dev->m_camlog_message);
+     if(debugLevel > 0) {
+        pr_debug("cameralog_write .");
     }
 
-    mutex_unlock(&camlog_dev->camlog_message_lock);
+    mutex_lock(&camlog_message_lock);
+    memset(&s_write_buf, 0 , sizeof(struct camlog_dev));
 
-    wake_up_interruptible(&camlog_dev->camlog_is_not_empty);
-
-    return bytes_read;
+    valid_len =  length >= MESSAGE_MAX?MESSAGE_MAX - 1:length;
+    ret = copy_from_user(s_write_buf.m_camlog_message, buff,  valid_len);
+    if (0 == ret)
+    {
+        bytes_wirte = strlen(s_write_buf.m_camlog_message);
+    }
+    if(debugLevel > 0) {
+         pr_debug("cameralog_write  valid_len:%d  strlen:%d.", valid_len, bytes_wirte);
+    }
+    if(0 == bytes_wirte)
+    {
+        //empty string!
+        mutex_unlock(&camlog_message_lock);
+        return 0;
+    }
+    if(debugLevel > 0) {
+        pr_debug("cameralog_write  input  msg : %s", s_write_buf.m_camlog_message);
+    }
+    //pr_debug("cameralog_write avail:%d", kfifo_avail(&message_kfifo) );
+    while (kfifo_avail(&message_kfifo) < sizeof(struct camlog_dev)) {
+        ret = kfifo_out(&message_kfifo, &s_read_buf, sizeof(struct camlog_dev ));
+         if (ret != sizeof(struct camlog_dev )) {
+            kfifo_skip(&message_kfifo);
+         }
+        //pr_debug("avail:%d", kfifo_avail(&message_kfifo) );
+    }
+    ret = kfifo_in(&message_kfifo, &s_write_buf, sizeof(struct camlog_dev ));
+     if (ret != sizeof(struct camlog_dev )) {
+        if(debugLevel > 0) {
+            pr_debug("cameralog: Failed to write to kfifo %d -  %d\n", ret, sizeof(struct camlog_dev ));
+        }
+        mutex_unlock(&camlog_message_lock);
+        return 0;
+    }
+    s_last_caller_uid = from_kuid(&init_user_ns, caller_task->cred->uid);
+    s_last_caller_pid = task_pid_nr(caller_task);
+    strncpy(s_last_message, s_write_buf.m_camlog_message, bytes_wirte);
+    if(debugLevel > 0) {
+        pr_debug("cameralog message kfifo   len:%d" , kfifo_len(&message_kfifo));
+    }
+    mutex_unlock(&camlog_message_lock);
+    wake_up_interruptible(&camlog_is_not_empty);
+    return bytes_wirte;
 }
-
 static int cameralog_release(struct inode *inode, struct file *filp)
 {
-    struct camlog_dev *camlog_dev = &camlogdev;
     pr_debug(" cameralog_release!\n");
-    wake_up_interruptible(&camlog_dev->camlog_is_not_empty);
+    wake_up_interruptible(&camlog_is_not_empty);
     return 0;
 }
-
 static unsigned int cameralog_poll(struct file *file,
     struct poll_table_struct *poll_table)
 {
     unsigned int mask = 0;
-    struct camlog_dev *camlog_dev = &camlogdev;
-    poll_wait(file, &camlog_dev->camlog_is_not_empty, poll_table);
-
-    if (strlen(camlog_dev->m_camlog_message))
+    poll_wait(file, &camlog_is_not_empty, poll_table);
+    if (kfifo_len(&message_kfifo) > 0)
     {
         mask = POLLIN | POLLRDNORM;
     }
-
     return mask;
 }
-
 static const struct file_operations cameralog_fops =
 {
     .owner = THIS_MODULE,
@@ -150,13 +298,17 @@ static const struct file_operations cameralog_fops =
     .read = cameralog_read,
     .write=cameralog_write,
 };
-
 static struct class *camlog_class;
 static unsigned int camlog_major;
 static int __init cameralog_init(void)
 {
     struct device *device;
-    struct camlog_dev *camlog_dev = &camlogdev;
+    int ret = 0;
+    ret = kfifo_alloc(&message_kfifo, KFIFO_COUNT*sizeof(struct camlog_dev), GFP_KERNEL);
+    if (ret) {
+        printk(KERN_ERR "cameralog message kfifo: Failed to allocate kfifo\n");
+        return ret;
+    }
     camlog_major = register_chrdev(0, CAMLOG_DEV_NAME, &cameralog_fops);
     camlog_class=class_create(THIS_MODULE, CAMLOG_DEV_NAME);
     if (IS_ERR(camlog_class)) {
@@ -164,31 +316,26 @@ static int __init cameralog_init(void)
         pr_warn("Failed to create class.\n");
         return PTR_ERR(camlog_class);
     }
-    camlog_dev->devt = MKDEV(camlog_major, 0);
-    device = device_create(camlog_class, NULL, camlog_dev->devt,
+    devt = MKDEV(camlog_major, 0);
+    device = device_create(camlog_class, NULL, devt,
                 NULL, CAMLOG_DEV_NAME);
-
     if (IS_ERR(device)) {
         pr_err("error while trying to create %s\n",CAMLOG_DEV_NAME);
         return -EINVAL;
     }
-    mutex_init(&camlog_dev->camlog_message_lock);
-    init_waitqueue_head(&camlog_dev->camlog_is_not_empty);
-
+    mutex_init(&camlog_message_lock);
+    init_waitqueue_head(&camlog_is_not_empty);
     return 0;
 }
-
 module_init(cameralog_init);
-
 static void __exit cameralog_exit(void)
 {
-    struct camlog_dev *camlog_dev = &camlogdev;
-    device_destroy(camlog_class, camlog_dev->devt);
+    device_destroy(camlog_class, devt);
     class_destroy(camlog_class);
     unregister_chrdev(camlog_major, CAMLOG_DEV_NAME);
+    kfifo_free(&message_kfifo);
 }
-
 module_exit(cameralog_exit);
-
 MODULE_DESCRIPTION("camera log device driver");
 MODULE_LICENSE("GPL v2");
+// END Camera_CameraSkyNet
